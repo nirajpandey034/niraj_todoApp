047b79861890b71660d8d7fa3a3d8bc38f41b498
Niraj Pandey <nirajpandey034@gmail.com>
2021-03-12 21:35:34 +0530

    finally

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,23 +6,17 @@ class todo extends Component {
     constructor(props) {
         super(props)
         this.state = {
-             item : [],
+             item : '',
              data : []
         }
     }
     addtolistHandler = () =>{
-            // let datas = [...this.state.data]
-
-            // datas.push(<Item_handler task={this.state.item}/>)
-
-            // let datas1 = datas.map((data_1,index)=>key={index})
-
-            // this.setState({
-            //     data:datas1
-            // })
+        if(this.state.item !== ''){
             this.setState({
-                data:<Item_handler task={this.state.item}/>
+                data: [...this.state.data,<Item_handler key={this.state.item} task={this.state.item}/>],
+                item :''
             })
+        }
     }
     //to get value from the input field
     itemadderHandler = (event) =>{
@@ -32,12 +26,10 @@ class todo extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="main">
                 <textarea className="textarea" value={this.state.item} onChange={this.itemadderHandler}></textarea><br />
-                <button className="buttons" onClick={this.addtolistHandler}>Add to list</button>
+                <button type="submit" className="buttons" onClick={this.addtolistHandler}>Add to list</button>
 
-                {/* {console.log(this.state.tasks) } */}
-                {/* {this.state.data} */}
                 {this.state.data}
             </div>
         )
